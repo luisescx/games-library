fec779a8544ca1f540addd53d8c4a63a956ecf34
luisescx <luis.esculapio@live.com>
2024-01-14 10:54:53 -0300

    feat: add game server api route

---
+++ src/server/api/routers/game.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+
+export const gameRouter = createTRPCRouter({
+  getGames: publicProcedure
+    .input(z.object({ page: z.number() }))
+    .query(async ({ ctx, input }) => {
+      const res = await ctx.dbGames.getGames(input.page);
+
+      return res;
+    }),
+});
