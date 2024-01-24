f02fc450a999552ae339a7d5af6752e2c2773f57
luisescx <luis.esculapio@live.com>
2024-01-24 19:33:36 -0300

    feat: add new queries to gameRouter

--- src/server/api/routers/game.ts
+++ src/server/api/routers/game.ts
@@ -25,4 +25,37 @@ export const gameRouter = createTRPCRouter({
         nextCursor: input.cursor! + 1,
       };
     }),
+  getGameDetails: publicProcedure
+    .input(
+      z.object({
+        slug: z.string(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const res = await ctx.apiGames.getGameDetails(input.slug);
+
+      return res;
+    }),
+  getGameScreenshots: publicProcedure
+    .input(
+      z.object({
+        slug: z.string(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const res = await ctx.apiGames.getGameScreenshots(input.slug);
+
+      return res;
+    }),
+  getGameSeries: publicProcedure
+    .input(
+      z.object({
+        slug: z.string(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const res = await ctx.apiGames.getGameSeries(input.slug);
+
+      return res;
+    }),
 });
