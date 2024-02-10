593cbe231b8cadde62b3f141a124f39903ad2aeb
luisescx <luis.esculapio@live.com>
2024-02-10 11:36:15 -0300

    chore: add tailwind forms in tailwind plugins

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,5 +10,8 @@ export default {
       },
     },
   },
-  plugins: [require("@tailwindcss/aspect-ratio")],
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+  ],
 } satisfies Config;
